42512ed5518ce0f1b6754ee512c65091de2470e8
koneone2002 <katieo3507@gmail.com>
2020-07-14 21:11:30 -0700

    user schema and user post added and working

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/', (req, res) =>
     msg: 'Welcome to the Caregiver Connect API ...'
   })
 );
+// define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
