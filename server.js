5e3dad7df2d99b0e9cc6aa02b593d4d46695b3bb
koneone2002 <katieo3507@gmail.com>
2020-07-14 18:50:44 -0700

    mongoDB and mongoose hooked up"

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
-app.get('/', (req, res) => res.send('API Running'));
+
+// Connect Database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+// app.get('/', (req, res) => res.send('API Running'));
+// const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) =>
+  res.json({
+    msg: 'Welcome to the Caregiver Connect API ...'
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
