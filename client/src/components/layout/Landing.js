adf28753b6a19780946c7cb1cd332493d57c9e2f
koneone2002 <katieo3507@gmail.com>
2020-07-17 14:10:03 -0700

    header and landing page:

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Landing = () => {
   return (
     <section className='landing'>
@@ -7,15 +7,15 @@ const Landing = () => {
         <div className='landing-inner'>
           <h1 className='x-large'>Caregiver Connector</h1>
           <p className='lead'>
-            Create caregiver profile, share posts and find help
+            Create a caregiver profile, share posts and find help
           </p>
           <div className='buttons'>
-            <a href='register.html' className='btn btn-primary'>
+            <Link to='/register' className='btn btn-primary'>
               Sign Up
-            </a>
-            <a href='login.html' className='btn'>
+            </Link>
+            <Link to='/login' className='btn'>
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
