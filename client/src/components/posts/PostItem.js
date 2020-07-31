46b4efc37aaa929fc0a6a2e7e52d2a0cbb6caeaf
koneone2002 <katieo3507@gmail.com>
2020-07-31 11:58:42 -0700

    fixed private routing issue

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,23 +1,14 @@
 import React, { Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
-import PostContext from '../../context/post/postContext';
+//import PostContext from '../../context/post/postContext';
 import AuthContext from '../../context/auth/authContext';
 import AlertContext from '../../context/alert/alertContext';
 
-const PostItem = props => {
-  const postContext = useContext(PostContext);
-  const {
-    post,
-    _id,
-    text,
-    name,
-    avatar,
-    user,
-    likes,
-    comments,
-    date
-  } = postContext;
+const PostItem = ({
+  post: { _id, text, name, avatar, user, likes, comments, date }
+}) => {
+  //const postContext = useContext(PostContext);
 
   const authContext = useContext(AuthContext);
   const { isAuthenticated } = authContext;
@@ -28,11 +19,7 @@ const PostItem = props => {
     <div className='post bg-white my-1 p-1'>
       <div>
         <a href='profile.html'>
-          <img
-            className='round-img'
-            src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
-            alt=''
-          />
+          <img className='round-img' src={avatar} alt='' />
           <h4>John Doe</h4>
         </a>
       </div>
