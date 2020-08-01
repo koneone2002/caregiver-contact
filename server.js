137728f32bac7999fbca87ac6a49c6717c9979fa
koneone2002 <katieo3507@gmail.com>
2020-08-01 11:09:29 -0700

    heroku setup

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
+const path = require('path');
+
 // Connect Database
 connectDB();
 
@@ -11,16 +13,26 @@ app.use(express.json({ extended: false }));
 // app.get('/', (req, res) => res.send('API Running'));
 // const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) =>
-  res.json({
-    msg: 'Welcome to the Caregiver Connect API ...'
-  })
-);
+// app.get('/', (req, res) =>
+//   res.json({
+//     msg: 'Welcome to the Caregiver Connect API ...'
+//   })
+// );
 // define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
