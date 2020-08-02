e66a8f132066ba5320688d718b365fa6c20923fc
koneone2002 <katieo3507@gmail.com>
2020-08-01 23:38:47 -0700

    still trying to fix

--- client/src/App.js
+++ client/src/App.js
@@ -23,8 +23,10 @@ const App = () => (
           <Router>
             <Fragment>
               <Navbar />
-              <Route exact path='/' component={Landing} />
-              <Route component={Routes} />
+              <Switch>
+                <Route exact path='/' component={Landing} />
+                <Route component={Routes} />
+              </Switch>
             </Fragment>
           </Router>
         </PostState>
